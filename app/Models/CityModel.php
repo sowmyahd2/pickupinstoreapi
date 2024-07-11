fa14f83ba80defa4ba1481f90a4039d3b52af420
sowmya <59165053+sowmyahd2@users.noreply.github.com>
2024-07-11 20:07:39 +0530

    fdfdf

--- app/Models/CityModel.php
+++
@@ -1,9 +0,0 @@
-<?php namespace App\Models;
-
-use CodeIgniter\Model;
-
-class CityModel extends Model
-{
-    protected $table         = 'citytable';
-    protected $returnType    = 'App\Entities\City';
-}
