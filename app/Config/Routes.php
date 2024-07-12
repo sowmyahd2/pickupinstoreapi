810c6786ab854f852cb9785457bb88d2424e389a
sowmya <59165053+sowmyahd2@users.noreply.github.com>
2024-07-12 09:40:26 +0530

    dsds

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -32,10 +32,8 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
-$routes->group('api/v1/', static function ($routes) {
-    $routes->get('department', ''Department::index);
-  
-});
+$routes->get('/v1/department', 'Department::index');
+
 
 /*
  * --------------------------------------------------------------------
